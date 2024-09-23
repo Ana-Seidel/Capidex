5e56a7a469a312c506875a0083ce79796dc6dc39
Ana Luiza Seidel <anaafsw1@gmail.com>
2024-09-23 14:15:14 -0300

    API routes

--- app.js
+++ app.js
@@ -6,10 +6,36 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 //Conectar ao banco de dados
-const db = new sqlite3.Database('./sqlite.db', (err) => {
+const db = new sqlite3.Database('./capivaras.db', (err) => {
     if (err) {
         console.error('Erro para conectar no banco de dados', err.message);
     } else {
         console.log('Conectado ao banco de dados');
     }
 });
+
+//Rotas da API
+app.get('/capivaras', (req, res) => {
+    db.all('SELECT * FROM capivaras', [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(rows);
+    });
+});
+
+app.post('/capivaras', (req, res) => {
+    const { nome, idade, peso, status_saude, habitat, comportamento, dieta, data_registro, observacoes } = req.body;
+    
+    const query = `
+        INSERT INTO capivaras (nome, idade, peso, status_saude, habitat, comportamento, dieta, data_registro, observacoes)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+    
+    db.run(query, [nome, idade, peso, status_saude, habitat, comportamento, dieta, data_registro, observacoes], function(err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(201).json({ id: this.lastID });
+    });
+}); 
